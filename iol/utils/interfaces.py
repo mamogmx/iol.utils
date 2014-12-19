b2a0205a970c3fafed798794b8682469142db692
mamogmx <marco.carbone@gmx.com>
2014-12-19 06:38:59 +0100

    test adapters

--- iol/utils/interfaces.py
+++ iol/utils/interfaces.py
@@ -1,4 +1,21 @@
 from zope.interface import Interface
 
 class IIolDocument(Interface):
-    pass
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def getIolRoles(self):
+        result = dict(
+            iol_owner=[],
+            iol_reviewer = [],
+            iol_manager = [],
+        )
+        for usr,roles in self.get_local_roles():
+            if 'Owner' in roles:
+                result['iol_owner'].append(usr)
+            if 'iol-reviewer' in roles:
+                result['iol_reviewer'].append(usr)
+            if 'iol-manager' in roles:
+                result['iol_manager'].append(usr)
+        return result
