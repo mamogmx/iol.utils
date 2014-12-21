247b2f850178558ad6ddb4caf9e755ab7cf7750e
mamogmx <marco.carbone@gmx.com>
2014-12-21 05:41:38 +0100

    test adapters

--- iol/utils/interfaces.py
+++ iol/utils/interfaces.py
@@ -1,34 +1,14 @@
 from zope.interface import Interface,implements
+from plone import api
 
 class IIolDocument(Interface):
-    """
-    Marker Interfaces
-    """
-
-class IolDocument(object):
-    implements(IIolDocument)
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
-
-    def getIolRoles2(self):
-        result = dict(
-            iol_owner = [],
-            iol_reviewer = [],
-            iol_manager = [],
-        )
-        for usr,roles in self.get_local_roles():
-            if 'Owner' in roles:
-                result['iol_owner'].append(usr)
-            if 'iol-reviewer' in roles:
-                result['iol_reviewer'].append(usr)
-            if 'iol-manager' in roles:
-                result['iol_manager'].append(usr)
-        return result
-
-    def getIolState2(self):
+    def getIolStatus(self):
         return api.content.get_state(obj=self)
+
+
 
 class IIolLayer(Interface):
     """Marker interface for the Browserlayer
     """
+
+
