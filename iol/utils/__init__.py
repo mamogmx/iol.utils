34023848bbb8106ca1c7bb0a004fd4247d2e8e9e
mamogmx <marco.carbone@gmx.com>
2014-12-21 15:14:26 +0100

    added create event

--- iol/utils/__init__.py
+++ iol/utils/__init__.py
@@ -1,5 +1,5 @@
 from zope.i18nmessageid import MessageFactory
-
+from AccessControl import allow_module
 
 # Set up the i18n message factory for our package
 MessageFactory = MessageFactory('iol.utils')
