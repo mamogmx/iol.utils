89eba514bff6e210fb923036cca4f8b85f9664ba
mamogmx <marco.carbone@gmx.com>
2014-12-21 15:21:34 +0100

    added create event

--- iol/utils/__init__.py
+++ iol/utils/__init__.py
@@ -1,7 +1,7 @@
 from zope.i18nmessageid import MessageFactory
 from zope.component import getGlobalSiteManager
 from .interfaces import IIolDocument,IolDocument
-from Product.CMFPlomino.interface import IPlominoDocument
+from Products.CMFPlomino.interfaces import IPlominoDocument
 # Set up the i18n message factory for our package
 MessageFactory = MessageFactory('iol.utils')
 gsm = getGlobalSiteManager()
