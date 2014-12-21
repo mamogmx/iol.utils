789407f704619f21ec6e7ca35ca8ac280d975f43
mamogmx <marco.carbone@gmx.com>
2014-12-21 09:01:30 +0100

    test adapters

--- iol/utils/handlers.py
+++ iol/utils/handlers.py
@@ -1,9 +1,12 @@
-__author__ = 'mamo'
+from Products.CMFPlomino.PlominoDocument import PlominoDocument
+from plone import api
+
+class IolDoc(PlominoDocument):
+    def getIolStatus(self):
+        return api.content.get_state(obj=self)
+
 
 def initIolDocument(doc,event):
-    """
+    doc = IolDoc(doc)
+    return doc
 
-    :param doc:
-    :param event:
-    :return:
-    """
