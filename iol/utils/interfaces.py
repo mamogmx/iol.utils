887f1e69cea242363ce801f12d211c3430d7bdf0
mamogmx <marco.carbone@gmx.com>
2014-12-21 06:32:00 +0100

    test adapters

--- iol/utils/interfaces.py
+++ iol/utils/interfaces.py
@@ -2,6 +2,11 @@ from zope.interface import Interface,implements
 from plone import api
 
 class IIolDocument(Interface):
+    """
+    marcker interface for iol document
+    """
+class IolDocument(object):
+    implements(IIolDocument)
     @property
     def getIolStatus(self):
         return api.content.get_state(obj=self)
