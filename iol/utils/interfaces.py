9766c31f61ac4fa0d2f4e7fab59d7d5edc22cf79
mamogmx <marco.carbone@gmx.com>
2014-12-22 02:03:00 +0100

    added create event

--- iol/utils/interfaces.py
+++ iol/utils/interfaces.py
@@ -1,13 +1,17 @@
 from zope.interface import Interface,implements
 from plone import api
+from AccessControl import ClassSecurityInfo
 
 class IIolDocument(Interface):
     """
     marcker interface for iol document
     """
 class IolDocument(object):
+    security = ClassSecurityInfo()
+
     implements(IIolDocument)
     @property
+    security.declarePublic('getIolStatus')
     def getIolStatus(self):
         return api.content.get_state(obj=self)
 
