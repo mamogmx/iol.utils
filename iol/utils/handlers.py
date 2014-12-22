749e898c7419999d1a9dc95129f9032ba39f5a4d
mamogmx <marco.carbone@gmx.com>
2014-12-22 06:58:07 +0100

    added create event

--- iol/utils/handlers.py
+++ iol/utils/handlers.py
@@ -5,6 +5,6 @@ from Products.CMFPlomino.interfaces import IPlominoDocument
 def initIolDocument(doc,event):
     import pdb
     pdb.set_trace()
-    adapter = IIolDocument(doc,IPlominoDocument,iol)
+    adapter = IIolDocument(doc,IPlominoDocument,'iol')
     doc.setItem('iol',adapter)
 
